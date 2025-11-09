5276ec18a724adb5353524de4f326905558a3a99
Omar Salloum <omarsalloum08@gmail.com>
2025-11-09 18:52:32 +0100

    adding more things but need to switch to another project nestjs3

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,20 @@ import { AppService } from './app.service';
 import { ArticlesModule } from './articles/articles.module';
 import { LoggerModule } from './logger/logger.module';
 import { EmployeesModule } from './employees/employees.module';
+import * as Joi from 'joi';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ArticlesModule, EmployeesModule, LoggerModule],
+  imports: [
+    ArticlesModule,
+    EmployeesModule,
+    LoggerModule,
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string().required(),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -3,10 +3,12 @@ import { LoggerService } from '../logger/logger.service';
 import { UniqueIdService } from '../unique-id/unique-id.service';
 import { Article } from './article';
 import { ArticleDto } from './articles.dto';
+import { PrismaService } from '../database/prisma.service';
 
 @Injectable()
 export class ArticlesService {
   constructor(
+    private readonly prismaService: PrismaService,
     private readonly loggerService: LoggerService,
     private readonly uniqueIdService: UniqueIdService,
   ) {}
@@ -58,9 +60,7 @@ export class ArticlesService {
 
   delete(id: string) {
     this.loggerService.log(`Delete article with id ${id}`);
-    const articleIndex = this.articles.findIndex(
-        (article) => article.id == id,
-    );
+    const articleIndex = this.articles.findIndex((article) => article.id == id);
     if (articleIndex === -1) {
       this.loggerService.warn(`No article with id ${id}`);
       throw new NotFoundException(`Article with id ${id} not found`);
